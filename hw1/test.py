19f5cf105b2ac4ca7fe42d4937b7375d221aef63
gordonmangum <gordonmangum@Gordons-Mac.local>
2015-11-20 17:42:40 -0500

    Initial Commit

---
+++ hw1/test.py
@@ -0,0 +1,10 @@
+from attempt1 import mcQuery
+
+def err():
+    mcQuery()
+    if output.mcQuery == 'Bees were mentioned more times in 2013'
+        print ('success!')
+    else
+        print ('there was a problem')
+
+err()
